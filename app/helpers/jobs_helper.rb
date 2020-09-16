21907c54b527652669ab1c56f89c8b8f03f0191d
Chris Coussa <ccoussa717@gmail.com>
2020-09-16 01:48:16 -0700

    Seed db, make formating work. Looking good.

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,12 @@
 module JobsHelper
-end
+
+    def job_type(job_type)
+      if job_type == "Full-time"
+        content_tag :span, "#{job_type}", class: "tag is-primary"
+      elsif job_type == "Part-time"
+        content_tag :span, "#{job_type}", class: "tag is-link"
+      else
+        ""
+      end
+    end
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,9 +22,7 @@ user = User.new(
     apply_url: "https://facebook.com/apply",
     job_type: "Full-time",
     location: "San Francisco, CA",
-    job_author: "Facebook",
     user_id: user.id,
-    avatar: Rails.root.join("app/assets/images/facebook.jpg").open,
     remote_ok: false
   },
   {
@@ -34,9 +32,7 @@ user = User.new(
     apply_url: "https://slack.com/apply",
     job_type: "Contract",
     location: "San Francisco, CA",
-    job_author: "Slack",
     user_id: user.id,
-    avatar: Rails.root.join("app/assets/images/slack.jpg").open,
     remote_ok: false
   },
   {
@@ -46,9 +42,7 @@ user = User.new(
     apply_url: "https://trello.com/apply",
     job_type: "Full-time",
     location: "Anywhere",
-    job_author: "Trello",
     user_id: user.id,
-    avatar: Rails.root.join("app/assets/images/trello.jpg").open,
     remote_ok: true
   },
   {
@@ -58,9 +52,7 @@ user = User.new(
     apply_url: "https://wordpress.org/apply",
     job_type: "Freelance",
     location: "Anywhere",
-    job_author: "WordPress",
     user_id: user.id,
-    avatar: Rails.root.join("app/assets/images/wordpress.jpg").open,
     remote_ok: true
   },
   {
@@ -70,9 +62,7 @@ user = User.new(
     apply_url: "https://basecamp.com/apply",
     job_type: "Full-time",
     location: "Chicago",
-    job_author: "Basecamp",
     user_id: user.id,
-    avatar: Rails.root.join("app/assets/images/basecamp.jpg").open,
     remote_ok: true
   },
   {
@@ -82,21 +72,7 @@ user = User.new(
     apply_url: "https://twitter.com/apply",
     job_type: "Part-time",
     location: "California",
-    job_author: "Twitter",
     user_id: user.id,
-    avatar: Rails.root.join("app/assets/images/twitter.jpg").open,
     remote_ok: true
   },
-  {
-    title: "Writer",
-    description: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Donec sed odio dui. Venenatis dapibus posuere. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna.",
-    url: "https://web-crunch.com",
-    apply_url: "https://web-crunch.com/apply",
-    job_type: "Full-time",
-    location: "St.Louis, Missouri",
-    job_author: "Web-Crunch",
-    user_id: user.id,
-    avatar: Rails.root.join("app/assets/images/web-crunch.jpg").open,
-    remote_ok: true
-  }
   ])
